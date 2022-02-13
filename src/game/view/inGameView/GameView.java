83af72cd5a84596ff61ccdb5981c04456529c488
jclegg03 <jjc2003@comcast.net>
2022-02-13 15:56:34 -0700

    added the utility panel to the view

--- src/game/view/inGameView/GameView.java
+++ src/game/view/inGameView/GameView.java
@@ -1,16 +1,30 @@
 package game.view.inGameView;
 
-import game.controller.Controller;
+import java.awt.GridLayout;
 
-public class GameView extends javax.swing.JPanel
+import javax.swing.JPanel;
+import javax.swing.SpringLayout;
+
+import game.controller.Controller;
+import gui.utility.JButton;
+
+public class GameView extends JPanel
 {
 	private Controller app;
 	private GameFrame frame;
+	private SpringLayout layout;
+	private JPanel utilityPanel;
+	private JButton researchButton;
+	private JButton menuButton;
 	
 	public GameView(Controller app, GameFrame frame)
 	{
 		this.app = app;
 		this.frame = frame;
+		this.layout = new SpringLayout();
+		this.utilityPanel = new JPanel();
+		this.researchButton = new JButton("Research");
+		this.menuButton = new JButton("Menu");
 		
 		setupPanel();
 		setupLayout();
@@ -20,11 +34,22 @@ public class GameView extends javax.swing.JPanel
 	private void setupPanel()
 	{
 		this.setFocusable(true);
+		
+		setupButtonPanel();
+		
+		this.add(utilityPanel);
+	}
+	
+	private void setupButtonPanel()
+	{
+		utilityPanel.setLayout(new GridLayout(0, 1, 0, 5));
+		utilityPanel.add(researchButton);
+		utilityPanel.add(menuButton);
 	}
 	
 	private void setupLayout()
 	{
-		
+		this.setLayout(layout);
 	}
 	
 	private void setupListeners()
