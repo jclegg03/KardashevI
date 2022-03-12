090d49db39331d44c49c5d488ee3ce5e45fc8ee8
jclegg03 <jjc2003@comcast.net>
2022-03-11 19:57:53 -0700

    Made the majority of the local biomes

--- src/game/model/lists/BiomeList.java
+++ src/game/model/lists/BiomeList.java
@@ -47,13 +47,14 @@ public class BiomeList
 		regionalBiomes.add(new RegionalBiome(new Color(10, 148, 111), "Reef", 10));
 		regionalBiomes.add(new RegionalBiome(new Color(133, 106, 25), "Shore", 10));
 		
-		regionalBiomes.add(new RegionalBiome(new Color(116, 130, 23), "Desert", 100));
+		regionalBiomes.add(new RegionalBiome(new Color(116, 130, 23), "Desert", 99));
+		regionalBiomes.add(new RegionalBiome(new Color(39, 0, 232), "Oasis", 1));
 		
 		regionalBiomes.add(new RegionalBiome(new Color(116, 181, 184), "Icebergs", 75));
 		regionalBiomes.add(new RegionalBiome(new Color(10, 10, 107), "Cold Deep Ocean", 25));
 		
 		regionalBiomes.add(new RegionalBiome(new Color(190, 196, 196), "Glacier", 10));
-		regionalBiomes.add(new RegionalBiome(new Color(75, 75, 75), "Ore Rich", 10));
+		regionalBiomes.add(new RegionalBiome(new Color(75, 75, 75), "Ore Rich", 10));//here
 		regionalBiomes.add(new RegionalBiome(new Color(76, 76, 76), "Stony", 80));
 		
 		regionalBiomes.add(new RegionalBiome(new Color(4, 117, 40), "Grasslands", 50));
@@ -62,6 +63,30 @@ public class BiomeList
 	
 	private void addLocalBiomes()
 	{
+		localBiomes.add(new LocalBiome(new Color(10, 10, 107), "Deep Ocean", 100));
+		
+		localBiomes.add(new LocalBiome(new Color(12, 94, 112), "Shallow Ocean", 100));
+		
+		localBiomes.add(new LocalBiome(new Color(10, 148, 111), "Reef", 100));
+		
+		localBiomes.add(new LocalBiome(new Color(133, 106, 25), "Shore", 100));
+		
+		localBiomes.add(new LocalBiome(new Color(116, 130, 23), "Desert", 99));
+		localBiomes.add(new LocalBiome(new Color(39, 0, 232), "Oasis", 1));
+		
+		localBiomes.add(new LocalBiome(new Color(39, 0, 232), "Water", 33));
+		localBiomes.add(new LocalBiome(new Color(97, 153, 14), "Fertile", 33));
+		localBiomes.add(new LocalBiome(new Color(116, 130, 23), "Sand", 34));
+		
+		localBiomes.add(new LocalBiome(new Color(116, 181, 184), "Ice", 90));
+		localBiomes.add(new LocalBiome(new Color(10, 10, 107), "Icy Ocean", 10));
+		
+		localBiomes.add(new LocalBiome(new Color(116, 181, 184), "Small Berg", 5));
+		localBiomes.add(new LocalBiome(new Color(10, 10, 107), "Cold Deep Ocean", 95));
+		
+		localBiomes.add(new LocalBiome(new Color(190, 196, 196), "Glacier", 99));
+		localBiomes.add(new LocalBiome(new Color(191, 197, 197), "Diamond", 1));
+		
 		
 	}
 }
