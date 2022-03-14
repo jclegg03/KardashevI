7dd88ba5f09ebe5296d819495ae95626274dd0ef
jclegg03 <jjc2003@comcast.net>
2022-03-13 18:24:36 -0600

    Marked local map methods as deprecated

--- src/game/model/maps/EmpireLocalMap.java
+++ src/game/model/maps/EmpireLocalMap.java
@@ -1,5 +1,6 @@
 package game.model.maps;
 
+import game.model.biomes.Biome;
 import game.model.empire.Empire;
 
 public class EmpireLocalMap extends EmpireMap
@@ -7,5 +8,16 @@ public class EmpireLocalMap extends EmpireMap
 	public EmpireLocalMap(Empire empire)
 	{
 		super(20, 20, empire);
+	}
+	
+	@Override @Deprecated
+	public EmpireMap getMap(int row, int col)
+	{
+		return null;
+	}
+
+	@Override @Deprecated
+	public void addMap(int row, int col, EmpireMap map)
+	{
 	}
 }
