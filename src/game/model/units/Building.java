d7aa1213a05064f5454a7303f8ac8e170e76eb2a
jclegg03 <jjc2003@comcast.net>
2022-05-13 12:41:29 -0600

    Doc for buildings

--- src/game/model/units/Building.java
+++ src/game/model/units/Building.java
@@ -2,6 +2,7 @@ package game.model.units;
 
 import java.io.Serializable;
 
+import game.controller.WIP;
 import game.model.resources.Resource;
 import game.model.resources.Tech;
 
@@ -10,29 +11,70 @@ import game.model.resources.Tech;
  * @author Jay Clegg
  *
  */
+@WIP
+//no constructor yet.
 public class Building implements Serializable, Describable, Requires
 {
-	private String name;
-	private Job[] employs;
-	private Resource[] produces;
+	/**
+	 * A list of resourses this consumer. Determined by the jobs.
+	 * @author Jay Clegg
+	 */
 	private Resource[] consumes;
-	private int size;
-	private Building upgradesTo;
+	
+	/**
+	 * A description of this building.
+	 * @author Jay Clegg
+	 */
 	private String description;
+	
+	/**
+	 * A list of jobs this employs.
+	 * @author Jay Clegg
+	 */
+	private Job[] employs;
+	
+	/**
+	 * The name of this building.
+	 * @author Jay Clegg
+	 */
+	private String name;
+	
+	/**
+	 * A list of resources this produces. Determined by the jobs.
+	 * @author Jay Clegg
+	 */
+	private Resource[] produces;
+	
+	/**
+	 * The tech requirement if any for this building.
+	 * @author Jay Clegg
+	 */
 	private Tech requirement;
+	
+	/**
+	 * The amount of tiles this building takes up.
+	 * @author Jay Clegg
+	 */
+	private int size;
+	
+	/**
+	 * What this can be upgraded to, if anything.
+	 * @author Jay Clegg
+	 */
+	private Building upgradesTo;
 	
 	public String getDescription()
 	{
 		return this.description;
 	}
 	
-	public void setDescription(String description)
-	{
-		this.description = description;
-	}
-	
 	public Tech getRequirement()
 	{
 		return this.requirement;
+	}
+	
+	public void setDescription(String description)
+	{
+		this.description = description;
 	}
 }
