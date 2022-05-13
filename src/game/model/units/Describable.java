b20cb2e786ab0eee3b9d84f054dea6ab230491fe
jclegg03 <jjc2003@comcast.net>
2022-05-13 12:42:01 -0600

    formated describable

--- src/game/model/units/Describable.java
+++ src/game/model/units/Describable.java
@@ -8,13 +8,14 @@ package game.model.units;
 public interface Describable
 {
 	/**
-	 * Sets the description of this object.
-	 * @param description The description of the object.
-	 */
-	public void setDescription(String description);
-	/**
 	 * Gives the description of this object.
 	 * @return The description of the object.
 	 */
 	public String getDescription();
+	
+	/**
+	 * Sets the description of this object.
+	 * @param description The description of the object.
+	 */
+	public void setDescription(String description);
 }
